b22c264ca7b24cee3b11d593598b5d981db1454a
George Gelashvili <ggelashvili@vmware.com>
2021-06-08 23:55:52 +0000

    split endpoints and cf manifest into ROLEs for pub/sub
    so each app only has subscribe/publish endpoints

--- app.rb
+++ app.rb
@@ -1,12 +1,13 @@
 require 'sinatra'
 require 'nats/client'
 
-nats_user = ENV['NATS_USER']
-nats_pass = ENV['NATS_PASS']
+NATS_USER = ENV['NATS_USER']
+NATS_PASS = ENV['NATS_PASS']
+ROLE = ENV['ROLE']
 
-client_cert_path = "#{Dir.pwd}/certs/client.crt"
-client_key_path = "#{Dir.pwd}/certs/client.key"
-ca_path = "#{Dir.pwd}/certs/ca.crt"
+CLIENT_CERT_PATH = "#{Dir.pwd}/certs/client.crt"
+CLIENT_KEY_PATH = "#{Dir.pwd}/certs/client.key"
+CA_PATH = "#{Dir.pwd}/certs/ca.crt"
 
 subscription_ids = []
 
@@ -28,68 +29,70 @@ NATS.on_close do
   EM.stop
 end
 
+def build_nats_options(host, port, subject)
+  options = {
+    :servers => [ "nats://#{NATS_USER}:#{NATS_PASS}@#{host}:#{port}" ],
+  }
+  if port == "4224"
+    options[:tls] = {
+      :private_key_file => CLIENT_KEY_PATH,
+      :cert_chain_file  => CLIENT_CERT_PATH,
+      :verify_peer => true,
+      :ca_file => CA_PATH,
+    }
+  end
+  options
+end
+
 get '/hello' do
   puts 'hi'
   STDERR.puts 'hello'
   return 'hi'
 end
 
-get '/publish/:host/:port/:subject' do |host, port, subject|
-  options = {
-    :servers => [ "nats://#{nats_user}:#{nats_pass}@#{host}:#{port}" ],
-  }
-
-  if port == "4224"
-    options[:tls] = {
-      :private_key_file => client_key_path,
-      :cert_chain_file  => client_cert_path,
-      :verify_peer => true,
-      :ca_file => ca_path,
-    }
-  end
-
-  NATS.start(options) do |nats|
-    STDERR.puts "Publish: Connected to NATS at #{nats.connected_server}"
-     nats.flush do
-       nats.publish(subject, "meow")
-       STDERR.puts "publishing!"
-     end
-
-  end
-end
-
-
-get '/subscribe/:host/:port/:subject' do |host, port, subject|
-  options = {
-    :servers => [ "nats://#{nats_user}:#{nats_pass}@#{host}:#{port}" ],
-  }
-
-  if port == "4224"
-    options[:tls] = {
-      :private_key_file => client_key_path,
-      :cert_chain_file  => client_cert_path,
-      :verify_peer => true,
-      :ca_file => ca_path,
-    }
-  end
-
-  NATS.start(options) do |nats|
-    STDERR.puts "Subscribe: Connected to NATS at #{nats.connected_server}"
-
-    sid = nats.subscribe(subject) do |msg|
-      STDERR.puts "Received from #{host}:#{port}: #{msg}"
+case ROLE
+when 'pub', 'publish', 'PUB', 'PUBLISH'
+  get '/publish/:host/:port/:subject' do |host, port, subject|
+    options = build_nats_options(host, port, subject)
+    NATS.start(options) do |nats|
+      STDERR.puts "Publish: Connected to NATS at #{nats.connected_server}"
+      nats.flush do
+        nats.publish(subject, 'meow')
+        STDERR.puts 'publishing!'
+      end
     end
-
-    subscription_ids.push(sid)
-
   end
 
-end
+when 'sub', 'subscribe', 'SUB', 'SUBSCRIBE'
+  get '/subscribe/:host/:port/:subject' do |host, port, subject|
+    options = build_nats_options(host, port, subject)
+    NATS.start(options) do |nats|
+      STDERR.puts "Subscribe: Connected to NATS at #{nats.connected_server}"
 
-get '/unsubscribe' do
-  subscription_ids.each do |sid|
-    NATS.unsubscribe(sid)
+      sid = nats.subscribe(subject) do |msg|
+        STDERR.puts "Received from #{host}:#{port}: #{msg}"
+      end
+      subscription_ids.push(sid)
+    end
   end
 
-  subscription_ids = []
+  get '/unsubscribe' do
+    subscription_ids.each do |sid|
+      NATS.unsubscribe(sid)
+    end
+    STDERR.puts "unsubscribed"
+
+    subscription_ids = []
+  end
+
+else
+  STDERR.puts 'ROLE environment variable must be set. Going down'
+  exit 2
 end
+
+
+get '/disconnect' do
+  NATS.stop
+  STDERR.puts "disconnected"
+end
+
