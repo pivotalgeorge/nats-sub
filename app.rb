4d168f2a882dcfb916103a7a2178b773c797b614
George Gelashvili <ggelashvili@vmware.com>
2021-06-08 22:47:32 +0000

    Add publishing functionality
    also read NATS creds from environment variable
    also support NATS TLS with certs in certs dir
    
    Co-authored-by: Amelia Downs <adowns@vmware.com>

--- app.rb
+++ app.rb
@@ -1,6 +1,15 @@
 require 'sinatra'
 require 'nats/client'
 
+nats_user = ENV['NATS_USER']
+nats_pass = ENV['NATS_PASS']
+
+client_cert_path = "#{Dir.pwd}/certs/client.crt"
+client_key_path = "#{Dir.pwd}/certs/client.key"
+ca_path = "#{Dir.pwd}/certs/ca.crt"
+
+subscription_ids = []
+
 # Set default callbacks
 NATS.on_error do |e|
   puts "Error: #{e}"
@@ -25,31 +34,62 @@ get '/hello' do
   return 'hi'
 end
 
-get '/connect/:host/:port/:subject' do |host, port, subject|
-
+get '/publish/:host/:port/:subject' do |host, port, subject|
   options = {
-    :servers => [
-     "nats://nats:<PASSWORD>@#{host}:#{port}"
-    ]
-    # :tls => {
-    #   :private_key_file => '/var/vcap/jobs/route_emitter/config/certs/nats/client.key',
-    #   :cert_chain_file  => '/var/vcap/jobs/route_emitter/config/certs/nats/client.crt',
-    #   :verify_peer => true,
-    #   :ca_file => '/var/vcap/jobs/route_emitter/config/certs/nats/ca.crt'
-    # }
+    :servers => [ "nats://#{nats_user}:#{nats_pass}@#{host}:#{port}" ],
   }
 
-  NATS.start(options) do |nats|
-    STDERR.puts "Connected to NATS at #{nats.connected_server}"
+  if port == "4224"
+    options[:tls] = {
+      :private_key_file => client_key_path,
+      :cert_chain_file  => client_cert_path,
+      :verify_peer => true,
+      :ca_file => ca_path,
+    }
+  end
 
-    nats.subscribe("*.*") do |msg|
-      STDERR.puts "Received: #{msg}"
+  NATS.start(options) do |nats|
+    STDERR.puts "Publish: Connected to NATS at #{nats.connected_server}"
+     nats.flush do
+       nats.publish(subject, "meow")
+       STDERR.puts "publishing!"
+     end
+
+  end
+end
+
+
+get '/subscribe/:host/:port/:subject' do |host, port, subject|
+  options = {
+    :servers => [ "nats://#{nats_user}:#{nats_pass}@#{host}:#{port}" ],
+  }
+
+  if port == "4224"
+    options[:tls] = {
+      :private_key_file => client_key_path,
+      :cert_chain_file  => client_cert_path,
+      :verify_peer => true,
+      :ca_file => ca_path,
+    }
+  end
+
+  NATS.start(options) do |nats|
+    STDERR.puts "Subscribe: Connected to NATS at #{nats.connected_server}"
+
+    sid = nats.subscribe(subject) do |msg|
+      STDERR.puts "Received from #{host}:#{port}: #{msg}"
     end
 
-  #  nats.flush do
-  #    nats.publish("hello", "world")
-  #  end
+    subscription_ids.push(sid)
+
   end
 
 end
 
+get '/unsubscribe' do
+  subscription_ids.each do |sid|
+    NATS.unsubscribe(sid)
+  end
+
+  subscription_ids = []
+end
